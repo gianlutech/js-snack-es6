3be90631b63f171089c9f192b9c583a1eba3a34f
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-21 09:57:04 +0100

    fine

--- js/snack1.js
+++ js/snack1.js
@@ -2,7 +2,7 @@ console.log('JS OK');
 /* Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal */
+Stampare in console la bici con peso minore utilizzando destructuring e template literal */
 
 const bikeList = [
     { nome: 'Specialized', peso: 30 },
@@ -10,4 +10,15 @@ const bikeList = [
     { nome: 'Decathlon', peso: 35 },
     { nome: 'Scott', peso: 28 }
 ]
-console.log(bikeList);
+console.log(bikeList.peso);
+let biciConPesoMinore = bikeList[0]
+for (let i = 0; i < bikeList.length; i++) {
+    const bike = bikeList[i];
+    const { peso } = bike;
+    if (peso < biciConPesoMinore.peso) biciConPesoMinore = bike;
+}
+console.log(biciConPesoMinore);
+
+// dati necessari : 1) Peso dell'oggetto su cui stiamo ciclando
+//2) il peso di biciConPesoMinore
+//3) Fare un paragone tra il peso di 1 e il peso di 2 (controllare se uno e' maggiore dell'altro)
